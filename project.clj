3a5106676980bde43ae06311953aa9c907b57c6e
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-03-20 11:18:49 +0800

    update package deps
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [coldnew/esunconsulting "0.1.0"]
-                 [morse "0.2.4"]
-                 [com.taoensso/timbre "4.7.4"]
-                 [migratus "0.8.32"]
-                 [im.chit/hara.io.scheduler "2.4.6"]
+                 [morse "0.4.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [migratus "1.0.6"]
+                 [im.chit/hara.io.scheduler "2.5.10"]
                  [im.chit/hara.time.joda "2.2.17"]
-                 [mount "0.1.10"]
+                 [mount "0.1.12"]
                  [yesql "0.5.3"]
                  [coldnew/config "1.2.0"]
-                 [org.clojure/core.async "0.2.395"]
-                 [org.xerial/sqlite-jdbc "3.14.2.1"]]
+                 [org.clojure/core.async "0.4.474"]
+                 [org.xerial/sqlite-jdbc "3.21.0.1"]]
 
   :plugins [[migratus-lein "0.2.6"]]
 
